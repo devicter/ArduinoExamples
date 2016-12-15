34cf07678ad51770c725ce1b688f3a9518a03750
Oleg Greengo <greengo@myttk.ru>
2016-12-15 15:44:49 +0700

    Update EnergyMonv1_with_Display_Nokia_2.0.0.ino

--- EnergyMonv1_with_Display_Nokia_2.0.0/EnergyMonv1_with_Display_Nokia_2.0.0.ino
+++ EnergyMonv1_with_Display_Nokia_2.0.0/EnergyMonv1_with_Display_Nokia_2.0.0.ino
@@ -21,7 +21,7 @@ and MySensors 2.0
 // Enable repeater functionality for this node
 //#define MY_REPEATER_FEATURE
 
-#define MY_RF24_CHANNEL  71
+#define MY_RF24_CHANNEL  XXX
 
 // Connected we have to move CE/CSN pins for NRF radio
 #define MY_RF24_CE_PIN   7
